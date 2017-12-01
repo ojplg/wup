6f813788a0bbdb89735fac5e8346bb5da4a54e23
ojplg <lees@ripco.com>
2017-11-30 20:24:11 -0600

    Convert to COW

--- wup.ml
+++ wup.ml
@@ -9,48 +9,21 @@ type exercise_set = { exercise : string;
                       weight : int }
 
 let squat_example = { exercise="Squat"; sets=5; reps_per_set=5; weight=155 }
-
-let render_attribute (name, value) =
-    name ^ "=" ^ value
- 
-let render_attributes attributes =
-    String.concat ~sep:" " (List.map attributes render_attribute)
-
-let render_element element attributes content =
-      "<" ^ element 
-          ^ (render_attributes attributes) ^ ">"
-          ^ content ^
-      "</" ^ element ^ ">"
-
-let build_item content = render_element "li" [] content
-
-let build_list items = render_element "ul"
-                                      []
-                                      (String.concat ~sep:"\n" items)
-
-let build_td content = render_element "td" [] content
-
-let date_data date = render_element "td"
-                                    ["class", "date"]
-                                    date
-
-let build_row data = render_element "tr"
-                                    []
-                                    (String.concat ~sep:"\n" data)
+let bench_example = { exercise="Bench"; sets=5; reps_per_set=5; weight=105 }
+let overhead_press_example = { exercise="Overhead Press"; sets=5; reps_per_set=5; weight=75 }
 
 let display set = set.exercise ^ " - " 
                                ^ string_of_int (set.sets * set.reps_per_set)
                                ^ "x"
                                ^ string_of_int set.weight
 
-let example_content = render_element "html" [] (render_element "body" [] 
-                        (build_row [date_data "27-Nov-2017";
-                                    build_td (build_list [build_item 
-                                             (display squat_example)])]))
+let sets_as_list sets = ul (List.map sets (fun s -> string (display s)))
 
 let cow_content = to_string @@ html 
                                @@ body 
-                                  @@ p @@ string "Hello"
+                                  @@ Create.table ~flags:[] 
+                                                  ~row:(fun s -> [string "27-Nov-2017"; sets_as_list s]) 
+                                                  [[squat_example; bench_example; overhead_press_example]]
 
 let app =
   App.empty |> (get "/" begin fun req ->
