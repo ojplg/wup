1717ebf07f832c9bbb8aa6e154e0be0b64c611e3
ojplg <lees@ripco.com>
2017-12-02 12:34:38 -0600

    Try to read data from DB

--- store.ml
+++ store.ml
@@ -13,9 +13,26 @@ let print_conn_info conn =
 
 let conn_str="host=localhost dbname=wup user=wupuser password=wupuserpass"
 
+(*
+let fetch_result c = wait_for_result c; c#get_result
+
+let fetch_single_result c =
+  match fetch_result c with
+  | None -> assert false
+  | Some r -> assert (fetch_result c = None); r
+*)
+
 let do_stuff =
   try
     let con = new connection ~conninfo:conn_str () in
-      print_conn_info con
-  with Postgresql.Error(m) -> print_endline ("postgres error \n " 
+      print_conn_info con;
+      let res = con#prepare "select trial" "select * from exercise_sessions" in
+        print_endline("prepared");
+        con#exec_prepared "select trial";
+        let num = res#fnumber "id" in
+          print_endline("found " ^ (string_of_int) num);
+        print_endline("and done");
+      (*   print_endline(" found record number " ^ (res#fname 0)) *)
+  with | Postgresql.Error(m) -> print_endline ("postgres error \n " 
                                                 ^ string_of_error(m))
+       | Not_found -> print_endline ("Something not found")
