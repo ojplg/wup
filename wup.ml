a4cad199718cb44f5ea70fffeed06e8a212d7ce6
ojplg <lees@ripco.com>
2017-12-01 20:48:56 -0600

    do not blindly open opium

--- wup.ml
+++ wup.ml
@@ -1,48 +1,46 @@
 open Core
-open Model
-open Opium.Std
 
-let squat_example = { exercise="Squat"; sets=5; reps_per_set=5; weight=155 }
-let bench_example = { exercise="Bench"; sets=5; reps_per_set=5; weight=105 }
-let barbell_row_example = { exercise="Barbell Row"; sets=5; reps_per_set=5; weight=75 }
-let overhead_press_example = { exercise="Overhead Press"; sets=5; reps_per_set=5; weight=75 }
-let deadlift_example = { exercise="Deadlift"; sets=2; reps_per_set=5; weight=135 }
+let squat_example = { Model.exercise="Squat"; sets=5; reps_per_set=5; weight=155 }
+let bench_example = { Model.exercise="Bench"; sets=5; reps_per_set=5; weight=105 }
+let barbell_row_example = { Model.exercise="Barbell Row"; sets=5; reps_per_set=5; weight=75 }
+let overhead_press_example = { Model.exercise="Overhead Press"; sets=5; reps_per_set=5; weight=75 }
+let deadlift_example = { Model.exercise="Deadlift"; sets=2; reps_per_set=5; weight=135 }
 
-let example_session_1 = { date="27-Nov-2017"; sets=[squat_example; bench_example; barbell_row_example] }
-let example_session_2 = { date="30-Nov-2017"; sets=[squat_example; overhead_press_example; deadlift_example] }
+let example_session_1 = { Model.date="27-Nov-2017"; sets=[squat_example; bench_example; barbell_row_example] }
+let example_session_2 = { Model.date="30-Nov-2017"; sets=[squat_example; overhead_press_example; deadlift_example] }
 
 let parse_set_from_request req = let params = Http.extract_query_parameters req in
-                                          { exercise=Http.find_string_param params "Movement";
+                                          { Model.exercise=Http.find_string_param params "Movement";
                                             sets=Http.find_int_param params "Sets";
                                             reps_per_set=Http.find_int_param params "Repetitions";
                                             weight=Http.find_int_param params "Weights" } 
                            
-let home_page_binding = get "/" 
+let home_page_binding = Opium.Std.get "/" 
                           begin
                             fun req -> Html.render (Html.home_page [example_session_1;
                                                                     example_session_2]) 
-                            |> respond'
+                            |> Opium.Std.respond'
                           end
 
-let new_set_binding = get "/new"
+let new_set_binding = Opium.Std.get "/new"
                         begin
                           fun req -> Html.render Html.set_form 
-                          |> respond'
+                          |> Opium.Std.respond'
                         end
 
-let submit_set_binding = get "/submitset"
+let submit_set_binding = Opium.Std.get "/submitset"
                            begin
-                             fun req -> Html.render (Html.simple_page (set_to_string (parse_set_from_request req)))
-                             |> respond'
+                             fun req -> Html.render (Html.simple_page (Model.set_to_string (parse_set_from_request req)))
+                             |> Opium.Std.respond'
                            end
 
 let app =
-  App.empty 
+  Opium.Std.App.empty 
   |> home_page_binding
   |> new_set_binding
   |> submit_set_binding
 
 let () =
   app
-  |> App.run_command
+  |> Opium.Std.App.run_command
 
