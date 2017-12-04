12b98182d22ca24119c8d52dcccd0afc140db3e6
ojplg <lees@ripco.com>
2017-12-04 16:37:07 -0600

    Add date picker to form

--- html.ml
+++ html.ml
@@ -16,14 +16,22 @@ let display set = set.Model.exercise
 
 let sets_as_list sets = ul (List.map sets (fun s -> string (display s)))
 
-let home_page ss = html 
-                    @@ body 
-                      (list
-                        [p (string "Welcome");
-                        Create.table ~flags:[] 
-                                      ~row:(fun session -> [string session.Model.date; 
-                                                            sets_as_list session.sets]) 
-                                      ss])
+let home_page ss = 
+      html 
+        @@ body 
+          (list
+            [p (string "Welcome");
+             Create.table ~flags:[] 
+               ~row:(fun session -> [string session.Model.date; 
+                                      sets_as_list session.sets]) 
+               ss;
+             p (string "Enter new session date below!");
+             tag "form" ~attrs:["id","new_session";"action","/submitsession"]
+               (list
+                 [tag "input" ~attrs:["type","date";"name","Date"] empty;
+                 tag "input" ~attrs:["type","submit";"value","Submit"] empty])
+            ]
+          )
 
 let weights = ["50";"55";"60";"65"]
 
