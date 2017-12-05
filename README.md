d0e9d29c1db22040c8c76a265a83712c4ce4b454
ojplg <lees@ripco.com>
2017-12-04 22:02:02 -0600

    Reformat

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ I do not understand the OCaml pakaging and build system, so things are even more
 
 # Random thoughts about OCaml
 
-1) Compiler seems lame. Only finds one error at a time and then gives up.
+1) Compiler seems lame. Only finds one error at a time and then gives up. Also seems to be only one-pass. It cares about the order of function definitions in a file.
 2) I loves me the pipe (|>) operator. More intuitive to me than the threading macros in Clojure.
 3) The way no-arg functions are considered constant and can run at start of a program is odd to me. Functions in OCaml are not always pure, so just because something has no arguments does not mean it always does the same thing, and since there is no equivalent of an IO Action a-la Haskell, it can get things screwed up. Very possible I am missing things here.
 4) The library for Postgres integration seems weakly typed. Why handle everything with strings? Java does much better here, though perhaps a different library does things more tightly. Again, I could be missing a lot of things here.
--- model.ml
+++ model.ml
@@ -1,17 +1,26 @@
 open Core
 
-type exercise_set = { exercise : string;
-                      session_id : int;
-                      sets : int;
-                      reps_per_set : int;
-                      weight : int }
+type exercise_set = 
+    { 
+      exercise : string;
+      session_id : int;
+      sets : int;
+      reps_per_set : int;
+      weight : int 
+    }
 
-let set_to_string set = "Set. " ^ set.exercise ^ " sets: " ^ (string_of_int set.sets)
-                                               ^ " reps_per_set: " ^ (string_of_int set.reps_per_set)
-                                               ^ " weight: " ^ (string_of_int set.weight)
+type exercise_session = 
+    { 
+      id : int;
+      date : string;
+      sets : exercise_set list 
+    }
 
-type exercise_session = { id : int;
-                          date : string;
-                          sets : exercise_set list }
+let set_to_string set = 
+    "Set. " ^ set.exercise ^ " sets: " ^ (string_of_int set.sets)
+      ^ " reps_per_set: " ^ (string_of_int set.reps_per_set)
+      ^ " weight: " ^ (string_of_int set.weight)
 
-let session_to_string sess = "Session. " ^ sess.date ^ String.concat ~sep:"\n" (List.map sess.sets set_to_string)
+let session_to_string sess = 
+    "Session. " ^ sess.date 
+     ^ String.concat ~sep:"\n" (List.map sess.sets set_to_string)
