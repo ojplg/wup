28e2a2e7e0d06d8bc7ae68ddef4014d00ab287ab
ojplg <lees@ripco.com>
2017-12-08 17:33:27 -0600

    Use library URL parser

--- wup.ml
+++ wup.ml
@@ -2,18 +2,21 @@ open Core
 
 let conn_str="host=localhost dbname=wup user=wupuser password=wupuserpass"
 
+let get_query_param req name =
+    (Opium.Std.Request.request req).resource 
+      |> Uri.of_string
+      |> Fn.flip Uri.get_query_param name
+      |> Util.opt_get
+ 
 let parse_set_from_request req = 
-    let params = 
-        Http.extract_query_parameters req in
-      { Model.exercise=Http.find_string_param params "Movement";
-        sets=Http.find_int_param params "Sets";
-        reps_per_set=Http.find_int_param params "Repetitions";
-        weight=Http.find_int_param params "Weights";
-        session_id = Http.find_int_param params "Session" } 
+    { 
+      Model.exercise = get_query_param req "Movement";
+      sets           = int_of_string(get_query_param req "Sets");
+      reps_per_set   = int_of_string(get_query_param req "Repetitions");
+      weight         = int_of_string(get_query_param req "Weights");
+      session_id     = int_of_string(get_query_param req "Session")
+    } 
                            
-let parse_date_from_request req = 
-    Http.find_string_param (Http.extract_query_parameters req) "Date"
-      
 let home_page con_str = 
     Store.find_all_sessions con_str
       |> Html.home_page
@@ -60,7 +63,7 @@ let submit_session_binding =
     begin
       fun req -> 
           req 
-        |> parse_date_from_request
+        |> Fn.flip get_query_param "Date"
         |> Store.insert_session conn_str
         |> home_or_error
         |> Html.render
